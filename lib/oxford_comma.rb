9f4146ffe3415cfeaaf5b3c5099f06a035eaa27b
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-05 22:51:13 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif
+    newArray = array.join(", ")
+    lastWord = array.last
+    lastLength = -lastWord.length - 1
+    newArray.insert(lastLength, "and ")
+  end
 end
